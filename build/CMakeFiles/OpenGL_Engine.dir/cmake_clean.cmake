8ee394afa65223c9a1d8702e2623e21825474b22
joardan <joardan.wibisana@gmail.com>
2025-03-07 20:39:29 +0800

    Rainbow_Rectangle

---
+++ build/CMakeFiles/OpenGL_Engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_Engine.dir/link.d"
+  "CMakeFiles/OpenGL_Engine.dir/main.cpp.o"
+  "CMakeFiles/OpenGL_Engine.dir/main.cpp.o.d"
+  "OpenGL_Engine"
+  "OpenGL_Engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL_Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
